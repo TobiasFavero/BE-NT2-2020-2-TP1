0eb21f63955a3aa52413691e92ddd80fcac4fb29
unknown <=>
2020-08-25 22:01:38 -0300

    update script

--- script.js
+++ script.js
@@ -9,6 +9,38 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-function addTodo() {
-  alert('Boton Add TODO clickeado!')
-}
+const targetCountHandler = (target) => {
+  let targetCount = Number(target.innerHTML);
+  
+  return {
+    get: () => { return targetCount },
+    increment: () => {
+      targetCount ++;
+      target.innerHTML = targetCount;
+    }
+  };
+};
+
+const addTodo = () => {
+  targetCountHandler(itemCountSpan).increment();
+  const node = newTodoNode(itemCountSpan);
+  list.appendChild(node);
+};
+
+const uncheckTodo = (e) => {
+  targetCountHandler(uncheckedCountSpan).increment();
+  const todoToRemove = document.querySelector(`#${e.target.id}`)
+  todoToRemove.remove();
+};
+
+const newTodoNode = (target) => {
+  const count = targetCountHandler(target).get();
+  const node = document.createElement('li');
+  const textNode = document.createTextNode(`TODO: ${count}`);
+  node.appendChild(textNode);
+  node.addEventListener('click', uncheckTodo);
+  node.setAttribute('id', `todo${count}`);
+  
+  return node;
+};
+
